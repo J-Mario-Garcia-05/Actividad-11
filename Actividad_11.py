79776ff0c9066a0d0d195e9e4255a91c1e540cb0
J-Mario-García <jm.gcaxaj@udeo.edu.gt>
2025-08-04 11:10:10 -0600

    Crear menú

--- Actividad_11.py
+++ Actividad_11.py
@@ -7,12 +7,31 @@ def quick_sort(nombres):
     mayores = [x for x in nombres if x > pivote]
     return quick_sort(menores) + iguales + quick_sort(mayores)
 nombres = []
-try:
-    cantidad = int(input("¿Cuántos nombres desea ingresar?: "))
-    for i in range(cantidad):
-        nombre = input(f"Ingrese el nombre {i + 1}: ")
-        nombres.append(nombre)
-except Exception as e:
-    print(f"Ha ocurrido un error: {e}")
-print("Nombres ingresados ordenados:")
-print(quick_sort(nombres))
+opcion = "0"
+while opcion != "3":
+    print("==MENÚ==")
+    print("1.Agregar nombres")
+    print("2.Mostrar nombres agregados")
+    print("3.Salir")
+    opcion = input("\nSeleccione una opción: ")
+    try:
+        match opcion:
+            case "1":
+                cantidad = int(input("¿Cuántos nombres desea ingresar?: "))
+                for i in range(cantidad):
+                    nombres.append(input(f"Ingrese el nombre {i + 1}: "))
+            case "2":
+                if not nombres:
+                    print("No ha ingresado ningún nombre")
+                    continue
+                print("Nombres agregados:")
+                for i in nombres:
+                    print(i, end= ", ")
+                print("\nNombres ingresados ordenados:")
+                print(quick_sort(nombres))
+            case "3":
+                print("Saliendo")
+            case __:
+                print("Opcion no disponible")
+    except Exception as e:
+        print(f"Ha ocurrido un error: {e}")
